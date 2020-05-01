45e44c83c20cf92b56159b24b583030646779ca5
Rohit Kr Singh <49774800+rsbhartipay@users.noreply.github.com>
2020-05-01 23:51:29 +0530

    Add files via upload

---
+++ OpenCart_V2.0-master/admin/language/english/payment/pg.php
@@ -0,0 +1,33 @@
+<?php
+// Heading
+$_['heading_title']         = 'PG';
+
+// Text
+$_['text_extension']        = 'Extensions';
+$_['text_success']          = 'Success: You have modified PG account details!';
+$_['text_edit']             = 'Edit PG';
+$_['text_PG']        = '<img src="view/image/payment/PG.png" alt="PG" title="PG" style="border: 1px solid #EEEEEE;" />';
+
+// Entry
+$_['entry_pay_id']          = 'PG Pay ID';
+$_['entry_salt']            = 'Salt';
+$_['entry_test']            = 'Test Mode';
+$_['entry_total']           = 'Total';
+$_['entry_order_status']    = 'Order Status';
+$_['entry_geo_zone']        = 'Geo Zone';
+$_['entry_status']          = 'Status';
+$_['entry_sort_order']      = 'Sort Order';
+
+// Help
+$_['help_salt']             = 'PG Salt';
+$_['help_total']            = 'The checkout total the order must reach before this payment method becomes active.';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify payment PG!';
+$_['error_pay_id']          = 'PG Pay ID required!';
+$_['error_salt']            = 'Salt required!';
+$_['text_yes']            = 'Yes';
+$_['text_no']            = 'No';
+$_['text_all_zones']            = 'Select Zone';
+$_['text_enabled']            = 'Enabled';
+$_['text_disabled']            = 'Disabled';
